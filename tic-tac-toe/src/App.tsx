31b9d44912194156bbe20ba293edfdaf99994220
Jonatas Ribeiro <jonatas@prottaribeiro.com>
2023-06-19 19:34:18 +0100

    basic style board

--- tic-tac-toe/src/App.tsx
+++ tic-tac-toe/src/App.tsx
@@ -1,11 +1,17 @@
 
 import './App.css'
+import TicTacToe from './components/TicTacToe'
 
 function App() {
 
   return (
     <>
-      
+      <header>
+        <h1>
+          Tic Tac Toe
+        </h1>
+      </header>
+      <TicTacToe />
     </>
   )
 }
---
+++ tic-tac-toe/src/components/TicTacToe.tsx
@@ -0,0 +1,13 @@
+import "./game.css";
+
+const TicTacToe = () => {
+    return <>
+        <main>
+            <div className="container-game">
+                
+            </div>
+        </main>
+    </>
+}
+
+export default TicTacToe
