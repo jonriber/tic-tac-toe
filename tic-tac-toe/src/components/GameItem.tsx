06093eb4e4c7fdb38616dd79368d89ce5f4fde01
Jonatas Ribeiro <jonatas@prottaribeiro.com>
2023-06-19 20:45:24 +0100

    card game style

---
+++ tic-tac-toe/src/components/GameItem.tsx
@@ -0,0 +1,11 @@
+import "./game.css"
+
+const GameItem = ({val}) => {
+    return <>
+        <div className="item">
+            {val}
+        </div>
+    </>
+}
+
+export default GameItem
--- tic-tac-toe/src/components/TicTacToe.tsx
+++ tic-tac-toe/src/components/TicTacToe.tsx
@@ -1,9 +1,16 @@
+import GameItem from "./GameItem";
 import "./game.css";
+const initialBoard = Array(9).fill(null)
 
 const TicTacToe = () => {
     return <>
         <main>
             <div className="container-game">
+                <div className="board-game">
+                    {initialBoard.map((item,index) => (
+                        <GameItem key={index} val={"x"}/>
+                    ))}
+                </div>
                 
             </div>
         </main>
